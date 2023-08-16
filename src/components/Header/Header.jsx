b754540085178b1b6f03abd58497e4088c65cac3
Elizaveta Burlakova <es.burlakova@mail.ru>
2023-08-16 22:42:26 +0300

    1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,65 @@
 import logo from "../../images/Mesto.svg";
+import { Link } from 'react-router-dom';
+import { useCallback, useState } from 'react';
 
-function Header() {
+function Header({ name, dataUser }) {
+  const [count, setCount] = useState(0)
+
+  const closeBurgerForResize = useCallback(() => {
+    if (document.documentElement.clientWidth > '767') {
+      setCount(0)
+      window.removeEventListener('resize', closeBurgerForResize)
+    }
+  }, [])
+
+  function handelClick() {
+    if (count === 0) {
+      setCount(1)
+      window.addEventListener('resize', closeBurgerForResize)
+    } else {
+      setCount(0)
+      window.removeEventListener('resize', closeBurgerForResize)
+    }
+  }
+
+  function onSignOut() {
+    setCount(0);
+    localStorage.removeItem('jwt')
+  }
+
   return (
-    <header className="header">
-      <img className="logo" src={logo} alt="Логотип сайта Место." />
+    <header className={`header page__header ${count !== 0 ? 'page__header_opened' : ''}`}>
+      <img
+        src={logo}
+        alt="логотип место"
+        className="header__logo"
+      />
+      {name === 'signup' || name === 'signin' ?
+        <Link to={name === 'signup' ? '/sign-in' : '/sign-up'} className="header__link">
+          {name !== 'signup' ? 'Регистрация' : 'Войти'}
+        </Link> :
+        <>
+          <div className={`header__email-container ${count !== 0 ? 'header__email-container_opened' : ''}`}>
+            <p className='header__email'>{dataUser}</p>
+            <Link to={`/sign-in`} className='header__unlogin' onClick={onSignOut}>Выйти</Link>
+          </div>
+          <button className={`header__button ${count !== 0 ? 'header__button_active' : ''}`} onClick={handelClick}></button>
+        </>
+      }
     </header>
-  );
+
+// function Header() {
+//   return (
+//     <header className="header">
+//       <img className="logo" src={logo} alt="Логотип сайта Место." />
+//     </header>
+//   );
+// }
+
+// export default Header;
+
+
+  )
 }
 
 export default Header;
